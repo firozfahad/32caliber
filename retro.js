1c2113eb03eb2271eec108bd33cdec865e235198
firozfahad <150683528+firozfahad@users.noreply.github.com>
2024-08-25 02:39:10 +0530

    Add files via upload

---
+++ retro.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function() {
+  const mainContent = document.querySelector('main p');
+  setInterval(function() {
+      mainContent.style.visibility = (mainContent.style.visibility === 'hidden' ? '' : 'hidden');
+  }, 500); // Blinks every 500ms
+});
